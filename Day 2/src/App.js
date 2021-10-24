10cdcd7960f78705c88cc0afc37304bd03b1bb56
Aniket Padmansh <pudmaansh@gmail.com>
2021-10-24 13:35:25 +0530

    Day 2

--- Day 2/src/App.js
+++ Day 2/src/App.js
@@ -1,15 +1,7 @@
 import "./App.css";
-import AddButton from "./Components/AddButton";
-import Card from "./Components/Card";
 
 function App() {
-  const color = "#0f9d58";
-  return (
-    <div>
-      <Card />
-      <AddButton color={color} />
-    </div>
-  );
+  return <div>Hello</div>;
 }
 
 export default App;
